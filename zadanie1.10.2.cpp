d417ca532532d3011718b851480340b3fcee4717
Konststep <Rain1-1@mail.ru>
2026-05-17 18:51:24 +0300

    Add files via upload

---
+++ zadanie1.10.2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+double create_array(int size) {
+    double* mas = new double[size]();
+    cout << "Массив: ";
+
+    for (int i = 0; i < size; i++) {
+        cout << mas[i] << " ";
+    }
+    delete[] mas;
+    return 0;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int size = 0;
+
+    cout << "Введите размер массива: ";
+    cin >> size;
+    create_array(size);
+
+    return EXIT_SUCCESS;
+}

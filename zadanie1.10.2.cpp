ea0317d227a9d9e098fd0b9c507b81e27d9e48e3
Konststep <Rain1-1@mail.ru>
2026-05-19 14:55:34 +0300

    Add files via upload

--- zadanie1.10.2.cpp
+++ zadanie1.10.2.cpp
@@ -5,16 +5,19 @@
 
 using namespace std;
 
-double create_array(int size) {
+double* create_array(int size) {
     double* mas = new double[size]();
+            
+    return mas;
+}
+void print_array(double mas[], int size) {
     cout << "Массив: ";
 
     for (int i = 0; i < size; i++) {
         cout << mas[i] << " ";
     }
     delete[] mas;
-    return 0;
-}
+ }
 
 int main() {
     setlocale(LC_ALL, "RUS");
@@ -23,9 +26,9 @@ int main() {
 
     int size = 0;
 
+    double* mas = create_array(100);
     cout << "Введите размер массива: ";
     cin >> size;
-    create_array(size);
-
+    print_array(mas, size);
     return EXIT_SUCCESS;
 }

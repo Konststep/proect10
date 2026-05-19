0024209f77c02a900842748afad0ca1c055ff2e8
Konststep <Rain1-1@mail.ru>
2026-05-19 15:43:59 +0300

    Add files via upload

--- zadanie1.10.1.cpp
+++ zadanie1.10.1.cpp
@@ -1,18 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 #include <clocale>
 #include <string>
 #include <Windows.h>
 
 using namespace std;
-
-int massiv(int mas[], int size) {
-    cout << "Введённый массив: ";
-
-    for (int i = 0; i < size; i++) {
-        cout << mas[i] << " ";
-    }
-    return 0;
-}
 
 int main() {
     setlocale(LC_ALL, "RUS");
@@ -20,15 +11,19 @@ int main() {
     SetConsoleOutputCP(1251);
 
     int size = 0;
-    int* mas = new int[size]();
+        
     cout << "Введите размер массива: ";
     cin >> size;
+    int* mas = new int[size]();
     for (int i = 0; i < size; i++) {
         cout << "MAS[" << i << "] = ";
         cin >> mas[i];
     }
+    cout << "Введённый массив: ";
 
-    massiv(mas, size);
-    //delete[] mas;  Почему то как только я (ракомментирую) очищаю массив у меня не доходит до return EXIT_SUCCESS а выдается ошибка,без очищения все хорошо
+    for (int i = 0; i < size; i++) {
+        cout << mas[i] << " ";
+    }
+    delete mas;
     return EXIT_SUCCESS;
 }
